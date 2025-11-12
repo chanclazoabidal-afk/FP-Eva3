3ee3878e67fd7dbdbfbdd26b6bc266ffbb5c1d02
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-11-11 20:12:18 -0800

    Add files via upload

---
+++ EVA3_8_SOBRECARGA/src/main/java/com/mycompany/eva3_8_sobrecarga/EVA3_8_SOBRECARGA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_8_sobrecarga;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA3_8_SOBRECARGA {
+
+   
+        public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println(100);
+        System.out.println(100.5);
+        System.out.println(true);
+        System.out.println('c');
+        System.out.println("args");
+    }
+}
